59231169cdd9d73be52d82308b8414d198dab5a0
Chiranth A J <chiranth@m.ms.evolution-x.org>
2022-08-06 23:49:32 +0300

    extra: Add support for Google Dialer Call recording
    [someone5678] Modularized
    
    Original commit:
    PixelExp-Project/vendor_aosp@c3b9f5c
    
    Signed-off-by: Chiranth A J <chiranth@m.ms.evolution-x.org>
    Change-Id: I88ddeb57aa54e4fad88c3c9cba331dbbd7b4bd59
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    Signed-off-by: Vitorgl2003 <vitorgl.2003@gmail.com>
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- extra.mk
+++ extra.mk
@@ -3,6 +3,10 @@ ifeq ($(Lineage_GAPPS),true)
 TARGET_UNOFFICIAL_BUILD_ID := GAPPS
 $(call inherit-product, vendor/gapps/common/common-vendor.mk)
 
+# Call recording for Google Dialer
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+
 PRODUCT_PACKAGES += \
     UpdaterGMSOverlay
 else
