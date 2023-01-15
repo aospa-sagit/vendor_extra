351825e53feb0f215148b9e5844158ae5ecdb0b2
Nanhumly <1607616422@qq.com>
2023-01-15 02:55:36 +0000

    extra: Add Gapps build support

--- extra.mk
+++ extra.mk
@@ -1,7 +1,15 @@
+# GAPPS
+ifeq ($(Lineage_GAPPS),true)
+TARGET_UNOFFICIAL_BUILD_ID := GAPPS
+$(call inherit-product, vendor/gapps/common/common-vendor.mk)
+
+PRODUCT_PACKAGES += \
+    UpdaterGMSOverlay
+else
+PRODUCT_PACKAGES += \
+    UpdaterOverlay
+endif
+
 # SystemUI
 PRODUCT_PACKAGES += \
     SystemUIOverlay
-
-# Updater
-PRODUCT_PACKAGES += \
-    UpdaterOverlay
